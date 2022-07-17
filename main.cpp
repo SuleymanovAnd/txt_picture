ddf1c838426ebe33d191ee4efacd08bb1e0e1372
Suleymanov Andrey <sasus37@gmail.com>
2022-07-17 17:51:34 +0300

    all done;

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    srand(time(nullptr));
+
+    ofstream file ("pic.txt", ios::app);
+
+    if (!file.is_open()) {
+    cout << "Error opening file!";
+    }
+
+    int x = 0;
+    int y = 0;
+    cout << "Enter the width and height of the picture: ";
+    cin >>x >>y;
+
+    for (int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            file << rand () %2 << " ";
+        }
+        file <<std::endl;
+    }
+    cout << "complete\n";
+    file.close ();
 }
